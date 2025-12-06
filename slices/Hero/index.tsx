6f76306561ea1af7fee2c609db4f72abf79333da
tusharkumar2302 <tusharkumar27864@gmail.com>
2025-12-06 18:58:42 +0530

    feat: add Tailwind CSS configuration with fluid design support and custom theme settings

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextLink } from "@prismicio/next";
+import { Bounded } from "@/app/components/Bounded";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.body} />
+      <PrismicNextLink field={slice.primary.button} />
+    </Bounded>
+  );
+};
+
+export default Hero;
